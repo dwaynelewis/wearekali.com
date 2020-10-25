b2370a51220f6fb3e70cff31ba962067aa648fb0
jbtristan <54360735+jbtristan@users.noreply.github.com>
2020-10-25 21:19:24 +0800

    Updated - Test Site

--- brittonearth/wp-includes/custom/js/custom.js
+++ brittonearth/wp-includes/custom/js/custom.js
@@ -1,20 +1,7 @@
-     // Once the video is ready
-    _V_("example_video_1").ready(function(){
-      
-      // Store the video object
-      var myPlayer = this; 
-      // Make up an aspect ratio
-      var aspectRatio = 385/700; 
-
-      function resizeVideoJS(){
-        var width = document.getElementById(myPlayer.id).parentElement.offsetWidth;
-        myPlayer.width(width).height( width * aspectRatio );
-
-      }
-      
-       // Initialize resizeVideoJS()
-      resizeVideoJS();
-      // Then on resize call resizeVideoJS()
-      window.onresize = resizeVideoJS; 
-
-    });
+var player = videojs('my-video', {
+	
+	controls: true,
+	poster: './wp-content/uploads/2020/10/video-cover-photo.jpg',
+	loop: true,
+	fluid: true
+});
